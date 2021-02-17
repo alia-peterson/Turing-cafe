22f54c12e4496cce28498f05ca6ac8b1f349f25b
Alia Peterson <alia@pnwebs.com>
2021-02-17 11:34:41 -0700

    Add test for typing into form

--- cypress/integration/cafe-spec.js
+++ cypress/integration/cafe-spec.js
@@ -12,4 +12,12 @@ describe('Turing Cafe', () => {
       .get('input[name="time"]')
       .get('input[name="guests"]')
   })
+
+  it('Should be able to enter reservation information', () => {
+    cy.get('h1').should('contain', 'Turing Cafe Reservations')
+      .get('input[name="name"]').type('Alia').should('have.value', 'Alia')
+      .get('input[name="date"]').type('02/25').should('have.value', '02/25')
+      .get('input[name="time"]').type('6:30').should('have.value', '6:30')
+      .get('input[name="guests"]').type(2).should('have.value', 2)
+  })
 })
