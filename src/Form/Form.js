d8b4f2d38de28f5619566266c77667d7ee1bf3fe
Alia Peterson <alia@pnwebs.com>
2021-02-17 10:08:11 -0700

    Add form styling

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -19,15 +19,42 @@ export default class Form extends Component {
 
   render() {
     return (
-      <article>
+      <section className='form-container'>
         <input
+          className='form-input'
+          className='form-input'
           type='text'
           name='name'
           placeholder='Name'
           value={this.state.name}
           onChange={this.handleChange}
           />
-      </article>
+        <input
+          className='form-input'
+          type='text'
+          name='date'
+          placeholder='Date (mm/dd)'
+          value={this.state.date}
+          onChange={this.handleChange}
+          />
+        <input
+          className='form-input'
+          type='text'
+          name='time'
+          placeholder='Time'
+          value={this.state.time}
+          onChange={this.handleChange}
+          />
+        <input
+          className='form-input'
+          type='text'
+          name='guests'
+          placeholder='Number of guests'
+          value={this.state.guests}
+          onChange={this.handleChange}
+          />
+        <button className='form-button'>Make Reservation</button>
+      </section>
     )
   }
 }
