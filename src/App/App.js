348247b5b20ad6900eed1d886520b6b9f7eab3ae
Alia Peterson <alia@pnwebs.com>
2021-02-17 09:21:41 -0700

    Setup file structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Form from '../Form/Form'
+import Reservations from '../Reservations/Reservations'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
-          
+          <Reservations />
         </div>
       </div>
     )
   }
 }
 
-export default App;
+export default App
---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Reservations.css'
+
+export default function Reservations() {
+  return (
+    <article>
+
+    </article>
+  )
+}
