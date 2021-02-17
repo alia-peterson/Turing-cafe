0d78bc6ae9920a872c98b0734818c528ab113337
Alia Peterson <alia@pnwebs.com>
2021-02-17 09:44:27 -0700

    Create reservation cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Card.css'
 
-export default function Card() {
+export default function Card({ name, date, time, number }) {
   return (
-    <article>
-
+    <article className='resy'>
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time} pm</p>
+      <p>Number of Guests: {number}</p>
+      <button className='resy-button'>Cancel</button>
     </article>
   )
 }
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Reservations.css'
 
-export default function Reservations() {
+export default function Reservations({ reservations }) {
+  const allReservations = reservations.map(resy => {
+    return (
+      <Card
+        key={resy.id}
+        name={resy.name}
+        date={resy.date}
+        time={resy.time}
+        number={resy.number}
+        />
+    )
+  })
   return (
-    <article>
-
+    <article className='resy-container'>
+      {allReservations}
     </article>
   )
 }
