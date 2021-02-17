4429cf4d6e53bf1b148b4606d791d5209dc8f1b5
Alia Peterson <alia@pnwebs.com>
2021-02-17 09:50:31 -0700

    Create initial form components

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Form.css'
 
-export default function Form() {
-  return (
-    <article>
+export default class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      guests: '',
+    }
+  }
 
-    </article>
-  )
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <article>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={this.state.name}
+          onChange={this.handleChange}
+          />
+      </article>
+    )
+  }
 }
